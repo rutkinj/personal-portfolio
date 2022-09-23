9ee3ceec8883166cc630ae84bbfb6282a592fb3d
Joe Rutkin <rutkinj@gmail.com>
2022-09-23 15:46:49 -0700

    personalizes about me info

--- src/components/About.js
+++ src/components/About.js
@@ -26,7 +26,7 @@ class About extends Component {
               <div className="polaroid">
                 <span style={{ cursor: "auto" }}>
                   <img
-                    height="250px"
+                    // height="250px"
                     src={profilepic}
                     alt="Avatar placeholder"
                   />
